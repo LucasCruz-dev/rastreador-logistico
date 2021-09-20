7b5e9617c75830f86eb6a8b454fe1085dbff6f3b
Lucas Cruz <lucascruz.contatoformal@gmail.com>
2021-09-20 00:07:00 -0300

    configurando Login

--- components/login.js
+++ components/login.js
@@ -1,24 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Platform, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 import {css} from '../assets/css/Css'
 
 
 export default function Login(){
-    return (
+    const [display, setDisplay]= useState('none');
+    const [user, setUser]= useState(null);
+    const [password, setPassword]= useState(null);
+    const [login, setLogin]= useState(null);
+
+    //Envio de Dados de Login
+    async function sendForm(){
+        let response = await fetch('http://192.168.0.102:3000/login',{
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ // os dados que serão enviados 
+              name: user,
+              password: password
+            })
+
+        });
+    }
+       
+        return (       
+
         <KeyboardAvoidingView
          behavior={Platform.OS === "ios" ? "padding" : "height"}
-         style={[css.container]}>
+         style={[css.container]}> 
             <View>
                 <Text>Imagem de Login</Text>
+                <Text>{user} - {password}</Text>
             </View>
             <View>
                 <Text style={css.login__msg}>Usuário ou Senha inválidos</Text>
             </View>
             <View style={css.login__form}>
-                <TextInput style={css.login__input} placeholder='Usuário' />
-                <TextInput style={css.login__input} placeholder='Senha' secureTextEntry={true} />
-                <TouchableOpacity style={css.login__button}>
+                <TextInput style={css.login__input} placeholder='Usuário' onChangeText={text=>setUser(text)}  /> 
+                <TextInput style={css.login__input} placeholder='Senha' secureTextEntry={true}  onChangeText={text=>setPassword(text)} />
+                <TouchableOpacity style={css.login__button} onPress={()=>sendForm()}>
                     <Text style={css.login__buttonText}> ENTRAR NO SISTEMA</Text>
                 </TouchableOpacity>
             </View>
--- controller.js
+++ controller.js
@@ -7,41 +7,16 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({extended:false})); //ver o que o urlencoded faz
-
+app.use(express.json()) // configura para aceitar o json vindo do front 
 let user=models.User;//importação das models
 let tracking=models.Tracking;//importação das models
 let product=models.Product;//importação das models
 
-
-//CRUD
-app.get('/create', async(req,res)=>{
-    let create = await user.create({ // o xxx.create, xxx=o nome da variavel que recebe toda a model
-        name: "Jane", 
-        password: "123",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-     });
-     res.send('Usuário criado com sucesso');
-});
-
-app.get('/read', async(req,res)=>{
-   let read = await user.findAll({raw:true});
-   res.send(read);
-});
-
-app.get('/update', async(req,res)=>{ //é possível, inclusive, dar update nas models relacionadas ao usuário em questão
-let update= await user.findByPk(1).then((response)=>{
-    response.name='Thiago Santos';
-    response.password='111';
-    response.save();
-});
-
-});
-
-app.get('/delete', async (req,res)=>{
-    user.destroy({
-        where:{id:2}
+app.post('/login', async (req,res)=>{
+    let response = await user.findOne({
+        where:{name: req.body.name, password: req.body.password}
     });
+    console.log(response);
 });
 
 
