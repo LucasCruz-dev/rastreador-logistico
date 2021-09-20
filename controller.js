eed46da07d9d4a67c0fc9b6c063265007d837189
Lucas Cruz <lucascruz.contatoformal@gmail.com>
2021-09-19 21:33:27 -0300

    CRUD

--- controller.js
+++ controller.js
@@ -13,7 +13,7 @@ let tracking=models.Tracking;//importação das models
 let product=models.Product;//importação das models
 
 
-//Criação de Usuários
+//CRUD
 app.get('/create', async(req,res)=>{
     let create = await user.create({ // o xxx.create, xxx=o nome da variavel que recebe toda a model
         name: "Jane", 
@@ -26,16 +26,29 @@ app.get('/create', async(req,res)=>{
 
 app.get('/read', async(req,res)=>{
    let read = await user.findAll({raw:true});
-   console.log(read);
+   res.send(read);
 });
 
+app.get('/update', async(req,res)=>{ //é possível, inclusive, dar update nas models relacionadas ao usuário em questão
+let update= await user.findByPk(1).then((response)=>{
+    response.name='Thiago Santos';
+    response.password='111';
+    response.save();
+});
 
+});
+
+app.get('/delete', async (req,res)=>{
+    user.destroy({
+        where:{id:2}
+    });
+});
 
 
 
 let port= process.env.PORT || 3000; //ver process.env.PORT 
 app.listen(port, (req,res)=>{
-    console.log('servidor rodando');
+    console.log("ok");
 });
 
 
