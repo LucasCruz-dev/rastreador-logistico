295c701851797bb4b2a6a029a336d8140f2b940a
unknown <lucascruz.contatoformal@gmail.com>
2021-11-02 10:44:52 -0300

    refazendo tela de login

--- assets/css/Css.js
+++ assets/css/Css.js
@@ -1,16 +1,42 @@
 import { StyleSheet } from "react-native";
 
 const css = StyleSheet.create({
-container: {
- 
- 
+    container: {
+ flex:1,
+ justifyContent:'center',
+ alignItems:'center',
+ backgroundColor:'#222'
 },
-form: {
-justifyContent:"center",
-textAlign:"center",
-alignItems:"center",
-backgroundColor:"#333"
-}
+
+login__msg:(text='none')=>({
+    fontWeight: "bold",
+    fontSize: 15,
+    color : 'red',
+    marginTop: 10,
+    marginBottom: 15,
+    display: text
+
+}),
+login__form: {
+width: "90%",
+},
+login__input: {
+    backgroundColor:"#fff",
+    fontSize:19,
+    padding: 10,
+    marginBottom:15
+
+    },
+
+    login__btn:{
+    padding: 22,
+    backgroundColor : "#f58634",
+    alignSelf:"center",
+    borderRadius:5    
+    },
+    login__btnText: {
+
+    }
 
 });
 export {css};
--- views/Login.js
+++ views/Login.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet, Platform, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Platform } from 'react-native';
+import { StyleSheet, Platform, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 import {css} from '../assets/css/Css'
 export default function Login(){
 return (
  <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={css.container} >
      <View >
          <View>
-             <Text style={css.login__msg}>Usuário ou senha inválidos</Text>
+             <Text style={css.login__msg()}>Usuário ou senha inválidos</Text>
          </View>
         <View style={css.login__form}>
             <TextInput style={css.login__input} placeholder='Usuário'/>
